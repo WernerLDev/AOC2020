9510ab241173bfab3b134868b8dccdddaa7129a0
WernerLDev <7613278+WernerLDev@users.noreply.github.com>
2020-12-02 19:26:37 +0100

    Formatting

--- src/day1.ts
+++ src/day1.ts
@@ -1,40 +1,36 @@
-import * as fs from 'fs';
+import * as fs from "fs";
 
-const input = 
-    fs
-      .readFileSync('inputs/day1.txt', 'utf-8')
-      .split('\n')
-      .filter(x => !!x)
-      .map(x => parseInt(x))
+const input = fs
+  .readFileSync("inputs/day1.txt", "utf-8")
+  .split("\n")
+  .filter((x) => !!x)
+  .map((x) => parseInt(x));
 
-
-const solvePart1 = (inp:number[]): number => {
-  
-  for(let i = 0; i < inp.length; i++) {
-    for(let j = 0; j < inp.length; j++) {
-      if(i != j && inp[i] + inp[j] == 2020) {
-        return inp[i] * inp[j]
+const solvePart1 = (inp: number[]): number => {
+  for (let i = 0; i < inp.length; i++) {
+    for (let j = 0; j < inp.length; j++) {
+      if (i != j && inp[i] + inp[j] == 2020) {
+        return inp[i] * inp[j];
       }
     }
   }
 
-  return 0
-}
+  return 0;
+};
 
-const solvePart2 = (inp:number[]): number => {
-  
-  for(let i = 0; i < inp.length; i++) {
-    for(let j = 0; j < inp.length; j++) {
-      for(let k = 0; k < inp.length; k++) {
-        if(i != j && j != k && inp[i] + inp[j] + inp[k] == 2020) {
-          return inp[i] * inp[j] * inp[k]
+const solvePart2 = (inp: number[]): number => {
+  for (let i = 0; i < inp.length; i++) {
+    for (let j = 0; j < inp.length; j++) {
+      for (let k = 0; k < inp.length; k++) {
+        if (i != j && j != k && inp[i] + inp[j] + inp[k] == 2020) {
+          return inp[i] * inp[j] * inp[k];
         }
       }
     }
   }
 
-  return 0
-}
+  return 0;
+};
 
-console.log(`Solution part 1: ${solvePart1(input)}`)
-console.log(`Solution part 2: ${solvePart2(input)}`)
+console.log(`Solution part 1: ${solvePart1(input)}`);
+console.log(`Solution part 2: ${solvePart2(input)}`);
--- src/day2.ts
+++ src/day2.ts
@@ -1,40 +1,35 @@
-import * as fs from 'fs';
+import * as fs from "fs";
 
-const parseInput = (input:string) => {
-    const regex = /^([0-9]+)\-([0-9]+)\ ([a-zA-Z])\:\ ([a-z]+)$/g
-    const parsed = regex.exec(input)
+const parseInput = (input: string) => {
+  const regex = /^([0-9]+)\-([0-9]+)\ ([a-zA-Z])\:\ ([a-z]+)$/g;
+  const parsed = regex.exec(input);
 
-    return parsed ? [parsed[1], parsed[2], parsed[3], parsed[4]] : []
-}
+  return parsed ? [parsed[1], parsed[2], parsed[3], parsed[4]] : [];
+};
 
-const input = 
-    fs
-      .readFileSync('inputs/day2.txt', 'utf-8')
-      .split('\n')
-      .map(x => parseInput(x))
+const input = fs
+  .readFileSync("inputs/day2.txt", "utf-8")
+  .split("\n")
+  .map((x) => parseInput(x));
 
+const solvePart1 = (lines: string[][]) => {
+  return lines.filter((line) => {
+    const [min, max, char, passwd] = line;
+    let n = 0;
+    for (let i = 0; i < passwd.length; i++) if (passwd[i] == char) n++;
+    return n >= parseInt(min) && n <= parseInt(max);
+  }).length;
+};
 
-const solvePart1 = (lines:string[][]) => {
-    return lines.filter(line => {
-        const [min, max, char, passwd] = line
-        let n = 0;
-        for(let i = 0; i < passwd.length; i++)
-            if(passwd[i] == char) n++;
-        return n >= parseInt(min) && n <= parseInt(max)
-    })
-    .length
-}
+const solvePart2 = (lines: string[][]) => {
+  return lines.filter((line) => {
+    const [min, max, char, passwd] = line;
+    const pos1 = passwd[parseInt(min) - 1];
+    const pos2 = passwd[parseInt(max) - 1];
 
-const solvePart2 = (lines:string[][]) => {
-    return lines.filter(line => {
-        const [min, max, char, passwd] = line
-        const pos1 = passwd[parseInt(min) - 1]
-        const pos2 = passwd[parseInt(max) - 1]
+    return (pos1 == char || pos2 == char) && pos1 != pos2;
+  }).length;
+};
 
-        return (pos1 == char || pos2 == char) && pos1 != pos2
-    })
-    .length;
-}
-
-console.log(`solution part 1: ${solvePart1(input)}`)
-console.log(`solution part 2: ${solvePart2(input)}`)
+console.log(`solution part 1: ${solvePart1(input)}`);
+console.log(`solution part 2: ${solvePart2(input)}`);
