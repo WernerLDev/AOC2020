67e455c38b2b062543d88332425146fb5b16d4c0
WernerLDev <7613278+WernerLDev@users.noreply.github.com>
2020-12-02 19:20:24 +0100

    Improved solution day 2

--- src/day2.ts
+++ src/day2.ts
@@ -1,27 +1,10 @@
 import * as fs from 'fs';
 
-
-type PasswdLine = {
-    n1:number,
-    n2:number,
-    char:string,
-    password: string
-}
-
 const parseInput = (input:string) => {
     const regex = /^([0-9]+)\-([0-9]+)\ ([a-zA-Z])\:\ ([a-z]+)$/g
+    const parsed = regex.exec(input)
 
-    const result = regex.exec(input)
-    if(result) {
-        return <PasswdLine>{
-            n1: parseInt(result[1]),
-            n2: parseInt(result[2]),
-            char: result[3],
-            password: result[4]
-        }
-    }
-
-    return null;
+    return parsed ? [parsed[1], parsed[2], parsed[3], parsed[4]] : []
 }
 
 const input = 
@@ -31,34 +14,27 @@ const input =
       .map(x => parseInput(x))
 
 
-
-const isPasswdValidPart1 = (line:PasswdLine) => {
-
-    let n = 0;
-
-    for(let i = 0; i < line.password.length; i++) {
-        if(line.password[i] == line.char) {
-            n++;
-        }
-    }
-
-    return n >= line.n1 && n <= line.n2;
+const solvePart1 = (lines:string[][]) => {
+    return lines.filter(line => {
+        const [min, max, char, passwd] = line
+        let n = 0;
+        for(let i = 0; i < passwd.length; i++)
+            if(passwd[i] == char) n++;
+        return n >= parseInt(min) && n <= parseInt(max)
+    })
+    .length
 }
 
+const solvePart2 = (lines:string[][]) => {
+    return lines.filter(line => {
+        const [min, max, char, passwd] = line
+        const pos1 = passwd[parseInt(min) - 1]
+        const pos2 = passwd[parseInt(max) - 1]
 
-const isPasswdValidPart2 = (line:PasswdLine) => {
-
-    const pos1 = line.password[line.n1 - 1]
-    const pos2 = line.password[line.n2 - 1]    
-
-    return (pos1 == line.char || pos2 == line.char) && pos1 != pos2
-
+        return (pos1 == char || pos2 == char) && pos1 != pos2
+    })
+    .length;
 }
 
-const solve = (lines:(PasswdLine | null)[]) => {
-    return lines
-            .filter(x => x && isPasswdValidPart2(x))
-            .length
-}
-
-console.log(solve(input))
+console.log(`solution part 1: ${solvePart1(input)}`)
+console.log(`solution part 2: ${solvePart2(input)}`)
