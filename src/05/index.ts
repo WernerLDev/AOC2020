7b28a040291a53782cffd618528d4bd17680b975
WernerLDev <7613278+WernerLDev@users.noreply.github.com>
2020-12-05 13:22:32 +0100

    Shorter and better solution for day 5

--- src/05/index.ts
+++ src/05/index.ts
@@ -1,29 +1,12 @@
 import * as fs from "fs";
 
-const processLine = (line: string) => {
-  const solve = (currIndex: number, min: number, max: number): number => {
-    const m = Math.floor((max + min) / 2);
-
-    if (!line[currIndex]) return m;
-    if (line[currIndex] == "F" || line[currIndex] == "L") {
-      return solve(currIndex + 1, min, m);
-    } else {
-      return solve(currIndex + 1, m + 1, max);
-    }
-  };
-
-  return solve(0, 0, 1023);
-};
-
-const ProcessSmarter = (line: string) =>
-  [...line].reduce<number>((number, nextVal) => {
-    return (number << 1) + (nextVal == "B" || nextVal == "R" ? 1 : 0);
-  }, 0);
+const processLine = (line: string) =>
+  parseInt([...line].map((c) => (c == "B" || c == "R" ? 1 : 0)).join(""), 2);
 
 const input = fs
   .readFileSync("inputs/day5.txt", "utf-8")
   .split("\n")
-  .map((line) => ProcessSmarter(line))
+  .map((line) => processLine(line))
   .sort((a, b) => b - a);
 
 const solvePart1 = () => {
